274409f5701eb3ee315b60990996c0d3ad23b8f2
Nana Yaw Donkor <100615967+Dconesoko@users.noreply.github.com>
2024-09-28 09:46:17 -0230

    Update streamlit_app.py
    selecting search_on values for inspection

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,7 @@ name_on_order = st.text_input('Name on Smoothie:','John Doe')
 st.write('The name on your Smoothie will be:',name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:',
@@ -31,6 +30,11 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
+        
+        ## SEARCH ON 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME']==fruit_chosen,'SEARCH_ON'].iloc[0]
+        st.write(f"The search value for {fruit_chosen} is {search_on}.")
+        
         ## FRUITY API CALL
         st.subheader(f"{fruit_chosen} Nutrition Information")
         fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_chosen}")
