9f65c2dea1367eac2758a645497b34ecec0093f2
Nana Yaw Donkor <100615967+Dconesoko@users.noreply.github.com>
2024-09-28 09:20:22 -0230

    Update streamlit_app.py
    convert st dataframe to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 
 cnx=st.connection("snowflake")
@@ -16,7 +17,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:','John Doe')
 st.write('The name on your Smoothie will be:',name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
